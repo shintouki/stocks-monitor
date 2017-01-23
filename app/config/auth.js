865d8495bcd74f7ea25b9b2e902da81864a5d50c
Corazon <tdyshin@gmail.com>
2017-01-23 13:59:33 -0500

    Removed auth and passport files because I will not be using oauth in this app

--- app/config/auth.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = {
-  'twitterAuth': {
-    'consumerKey': process.env.TWITTER_KEY,
-    'consumerSecret': process.env.TWITTER_SECRET,
-    'callbackURL': process.env.APP_URL + 'auth/twitter/callback'
-  }
-};
