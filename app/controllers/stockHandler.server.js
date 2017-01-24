90b42a31f0dc0aa1a3bf46e0483767f6801f11ca
Corazon <tdyshin@gmail.com>
2017-01-24 18:49:08 -0500

    Set up json call to quandl in add-stock on server side

--- app/controllers/stockHandler.server.js
+++ app/controllers/stockHandler.server.js
@@ -51,7 +51,50 @@ function StockHandler() {
   }
 
   this.addStock = function (req, res) {
-    
+    var stockCode = req.body.stockCode;
+
+    // Use Date() to get today's date
+    var today = new Date();
+    var year = today.getFullYear();
+    var month = today.getMonth() + 1;
+    var day = today.getDate();
+
+    var stockCode = req.query.stockName;
+    var startDate = year - 1 + '-' + month + '-' + day;
+    var endDate = year + '-' + month + '-' + day;
+    var apiKey = process.env.QUANDL_API_KEY
+
+    var quandlApiUrl = 'https://www.quandl.com/api/v3/datasets/WIKI/' + stockCode + '.json?api_key=' + apiKey + '&start_date=' + startDate + '&end_date=' + endDate;
+
+    getJSON(quandlApiUrl, function (err, data) {
+      if (err) {
+        console.log("Error");
+      }
+      else {
+        var details = data.dataset.name;
+        var stockPricesArr = data.dataset.data;
+        var relevantDataArr = [];
+        
+        for (var i = stockPricesArr.length - 1; i >= 0; i--) {
+          var indivDate = stockPricesArr[i][0];
+          var unixTime = new Date(indivDate).getTime();
+          var endPrice = stockPricesArr[i][4];
+
+          relevantDataArr.push([unixTime, endPrice]);
+        }
+
+        var resObj = {
+          stockDetails: details,
+          relevantDataArr: relevantDataArr
+        };
+
+        // Add stock to database
+
+        res.json(resObj);
+      }
+      
+    });
+
   }
 
   
