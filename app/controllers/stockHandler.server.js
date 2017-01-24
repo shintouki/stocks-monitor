c07bfed4e83c444865f8c18c214a764b86aba019
Corazon <tdyshin@gmail.com>
2017-01-24 12:13:43 -0500

    Set up stockhandler

---
+++ app/controllers/stockHandler.server.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var Stocks = require('../models/stocks.js');
+
+var appUrl = process.env.APP_URL
+
+function StockHandler() {
+  
+  this.stockSearch = function(req, res) {
+    
+  }
+  
+}
+
+module.exports = StockHandler;
