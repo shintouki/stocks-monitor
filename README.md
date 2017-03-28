b18692b5dbc9ef8a54864f5e7e7fff84a966dbd1
Corazon <tdyshin@gmail.com>
2017-03-28 14:45:03 -0400

    Add comments to server.js

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ https://monitor-stocks.herokuapp.com/
 
 Chart was creatded with highstock.js. Stocks data is from QUANDL Api. I used socket.io to use Web Sockets to see changes in real time when any other user adds or removes a stock. 
 
-Tech Stack: express, node, socket.io, mongoose, mongodb, jquery, bootstrap, pug
+Tech Stack: express, node, socket.io, mongoose, mongodb, jquery, bootstrap, pug.
--- server.js
+++ server.js
@@ -8,15 +8,20 @@ var bodyParser = require('body-parser');
 
 var app = express();
 var server = require('http').Server(app);
+
+// socket.io middleware
 var io = require('socket.io')(server);
 
 // require('dotenv').load();
 
+// mongoose middleware for mongodb
 mongoose.connect(process.env.MONGO_URI);
 
+// pug template engine middleware
 app.set('views', process.cwd() + '/public/pug')
 app.set('view engine', 'pug')
 
+// server static files with express static middleware funciton
 app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 app.use('/common', express.static(process.cwd() + '/app/common'));
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -24,15 +29,18 @@ app.use('/jquery', express.static(process.cwd() + '/node_modules/jquery/dist'));
 app.use('/bootstrap/js', express.static(process.cwd() + '/node_modules/bootstrap/dist/js'));
 app.use('/bootstrap/css', express.static(process.cwd() + '/node_modules/bootstrap/dist/css'));
 
+// express session middleware setup
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true
 }));
 
+// bodyparser middle so i can use req.body, which is used with post requests
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// routes, send in express and io middleware as parameters
 routes(app, io);
 
 var port = process.env.PORT || 8080;
